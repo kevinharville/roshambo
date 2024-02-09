8c00b8828353d50025cc9c06a24d454dc03a461c
Kevin Harville <kevinharville@Kevins-MacBook-Pro.local>
2024-02-08 16:09:51 -0800

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+res.json({thing: 'rock'});
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+module.exports = router;
