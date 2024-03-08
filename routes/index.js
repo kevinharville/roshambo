277dbdd348bbc493a2558b7ee86ab7cd39055c95
Kevin Harville <kevinharville@Kevins-MacBook-Pro.local>
2024-03-08 10:52:28 -0800

    changed data

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/hello', function(req, res, next) {
-res.json({thing: 'rock'});
+res.json({thing: 'monkey'});
 });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Server' });
 });
 module.exports = router;
